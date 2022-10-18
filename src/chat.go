0f56bd4db9f73c43d00dfe3067cf10207cc19f11
Aeonn <theodebauvais@protonmail.com>
2022-10-18 17:52:01 +0200

    some updates

--- src/chat.go
+++ src/chat.go
@@ -6,7 +6,19 @@ import (
 	"net"
 )
 
-func handle_client_as_server(client net.Conn) {
+func handle_client_logic(client net.Conn) {
+	defer client.Close()
+}
+
+func connect_as_client(client_string string) net.Conn {
+	conn, err := net.Dial("tcp", client_string) // appell TCP sur l'ip:port
+	if err != nil {                             // verification des erreurs
+		if conn != nil {
+			conn.Close() // ferme la connexion si err == nil et que la connexion est initialisee, permet de fermer proprement
+		}
+		log.Panic("Could not connect to " + client_string)
+	}
+	return conn
 }
 
 func wait_in_server_mode(server_mode_confirmed chan bool, client_mode_confirmed chan bool, client_accepted chan net.Conn, server net.Listener) {
@@ -24,7 +36,6 @@ func wait_in_server_mode(server_mode_confirmed chan bool, client_mode_confirmed 
 				continue
 			}
 		}
-		log.Println("Accepted from " + conn.RemoteAddr().String())
 		server_mode_confirmed <- true
 		client_accepted <- conn
 	}
@@ -52,11 +63,12 @@ func main() {
 
 	// for the server
 	server_mode_confirmed := make(chan bool)
-	// used when in server mode to send to the main part the client object
+	// used when in server mode to send to the main part the client's connection object
 	client_accepted := make(chan net.Conn)
 
 	// for the client
 	client_mode_confirmed := make(chan bool)
+	// used when in client mode to send to the main part the client's host:port
 	client_string := make(chan string)
 
 	go wait_in_server_mode(server_mode_confirmed, client_mode_confirmed, client_accepted, server)
@@ -64,8 +76,9 @@ func main() {
 
 	select {
 	case <-server_mode_confirmed:
-		handle_client_as_server(<-client_accepted)
+		handle_client_logic(<-client_accepted)
 	case <-client_mode_confirmed:
 		server.Close()
+		handle_client_logic(connect_as_client(<-client_string))
 	}
 }
