5e9a84fdc3a9b062e45e25552ce753c6a574f40d
astalios <37106010+Astalios@users.noreply.github.com>
2022-10-18 10:48:07 +0200

    premiere connexion

--- src/go-reverseShell.go
+++ src/go-reverseShell.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func shell( /*host string*/ ) {
+
+	conn, err := net.Dial("tcp", "127.0.0.1:7777")
+	if err != nil {
+		fmt.Println("erreur de connexion a l'hote")
+		os.Exit(1)
+	}
+	fmt.Fprintf(conn, "HelloWorld")
+}
 
 func main() {
-	fmt.Println("vim-go")
+
+	shell()
+
 }
